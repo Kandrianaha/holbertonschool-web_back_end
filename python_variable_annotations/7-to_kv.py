71820768480383cc86812c125f473eb5f89672b1
Kandrianaha <k.andria@icloud.com>
2026-03-24 19:26:57 +1100

    string and int/float to tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,7 +2,7 @@
 """This module contains a type-annotation
 function to_kv that takes a string and
 an int or float and returns a tuple"""
-from typing import Union, tuple
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
