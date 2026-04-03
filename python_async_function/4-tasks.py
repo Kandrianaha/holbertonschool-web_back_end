a6820afe63b61a52374fb124202c33677e9fe25f
Kandrianaha <k.andria@icloud.com>
2026-04-03 16:32:04 +1100

    deleting whitespace

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,13 +6,7 @@ import random
 from typing import List
 
 
-
-async def task_wait_random(max_delay: int) -> float:
-    """Wait for a random delay between 0 and max_delay and return the delay."""
-    delay = random.random() * max_delay
-    await asyncio.sleep(delay)
-    return delay
-
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
