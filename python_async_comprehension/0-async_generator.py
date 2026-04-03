725be2740d49c93738522199ddb5f9d4d30f31e5
Kandrianaha <k.andria@icloud.com>
2026-04-03 16:55:25 +1100

    pycodestyle comment

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,7 @@ async def async_generator():
     """async generator that loops 10 times
     waiting 1 second each time"""
     for _ in range(10):
-        #waits for 1 second
+        # waits for 1 second
         await asyncio.sleep(1)
-        #yield random bumber between 1 to 10
+        # yield random bumber between 1 to 10
         yield random.randint(0, 10)
