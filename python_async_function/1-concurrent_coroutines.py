18e771cbbf4466d874a51aa36148e7cfccaef8f7
Kandrianaha <k.andria@icloud.com>
2026-04-03 15:52:44 +1100

    fixed mistake on import line

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,12 +2,13 @@
 """This module imports wait_random and writes an async routine
 called wait_n"""
 import asyncio
+from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay:int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawns wait_random n times with the specified max_delay"""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delays = []
