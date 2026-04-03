b903a951ab1341385358e09576bf4c569d85f748
Kandrianaha <k.andria@icloud.com>
2026-04-03 16:40:36 +1100

    deleting whitespace

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,6 +2,7 @@
 """This module takes the code from wait_n and alter it
 to a new function task_wait_n"""
 import asyncio
+import random
 from typing import List
 
 
@@ -10,9 +11,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """returns a list of all delays in the order they finished"""
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
-    results: List[float] = []
-    for coro in asyncio.as_completed(tasks):
-        res = await coro
-        results.append(res)
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
     return results
