293ba2d353d59e7766cb9c64f3923cad91f0592c
Kandrianaha <k.andria@icloud.com>
2026-04-18 09:50:14 +1000

    pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Simple pagination"""
-
 import csv
 import math
 from typing import List
@@ -15,8 +14,7 @@ def index_range(page, page_size):
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
