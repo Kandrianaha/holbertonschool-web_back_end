12318fe4ff8291944ef9e5216add8332c9c21083
Kandrianaha <k.andria@icloud.com>
2026-05-03 16:43:53 +1000

    fixing the code so it handles the right array

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,20 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName)
-    ]).then((results) =>
-        results.map((result) => ({
-            status: result.status,
-            value: result.value || result.reason,
-        }))
-    )
+  const promiseA = signUpUser(firstName, lastName);
+  const promiseB = uploadPhoto(fileName);
+
+  return Promise.allSettled([promiseA, promiseB]).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return result;
+      }
+
+      return {
+        status: 'rejected',
+        value: result.reason.toString(),
+      };
+    })
+  );
 }
