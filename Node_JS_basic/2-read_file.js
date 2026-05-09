1d6d1c9e1ee609eff3f0b9759e9056a3159f0df3
Kandrianaha <k.andria@icloud.com>
2026-05-09 11:49:37 +1000

    Deleting trialspaces

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,26 +1,26 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function countStudents(path) {
   try {
     // Read file synchronously with utf-8 encoding
     const data = fs.readFileSync(path, 'utf8');
-    
+
     // Split lines and filter out empty lines
     const lines = data.split('\n').filter(line => line.trim() !== '');
-    
+
     // Remove the header line
     const studentLines = lines.slice(1);
-    
+
     console.log(`Number of students: ${studentLines.length}`);
-    
+
     const fields = {};
-    
+
     // Process each student line
     studentLines.forEach(line => {
       const student = line.split(',');
       const firstname = student[0];
       const field = student[3];
-      
+
       if (field) {
         if (!fields[field]) {
           fields[field] = [];
@@ -28,7 +28,7 @@ function countStudents(path) {
         fields[field].push(firstname);
       }
     });
-    
+
     // Log statistics for each field
     for (const field in fields) {
       if (Object.prototype.hasOwnProperty.call(fields, field)) {
