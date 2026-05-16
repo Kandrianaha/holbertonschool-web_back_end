492d57fcf7e2eb8bf8443aec974633418635ccd0
Kandrianaha <k.andria@icloud.com>
2026-05-16 11:25:26 +1000

    fixing typo

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -7,12 +7,12 @@ export const readDatabase = (filePath) => new Promise((resolve, reject) => {
     const lines = data.split('\n').filter((line) => line.trim() != '');
     const headers = lines[0].split(',');
     const fieldIndex = headers.indexOf('field');
-    const fistnameIndex = headers.indexOf('fistname');
+    const firstnameIndex = headers.indexOf('firstname');
 
     const result = {};
     lines.slice(1).forEach((line) => {
       const values = line.split(',');
-      const field = value[fieldIndex];
+      const field = values[fieldIndex];
       const firstname = values[firstnameIndex];
       if (!result[field]) result[field] = [];
       result[field].push(firstname);
