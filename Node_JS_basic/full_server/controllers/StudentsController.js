35ccc14f8a9de522c8218e9956b19b66d665438f
Kandrianaha <k.andria@icloud.com>
2026-05-16 11:09:59 +1000

    Adding full server taskj 8

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,31 @@
+import { readDatabase } from '../utils';
+
+export default class StudentsController {
+  static getAllStudents(request, response) {
+    readDatabase(process.argv[2])
+      .then((Data) => {
+        const fields = Object.key(data).sort((a, b) =>
+          a.toLowerCase().localeCompare(b.toLowerCase())
+      );
+      let output = 'This is the list of our students\n';
+      fields.forEach((field) => {
+        output += `Number of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}\n`;
+      });
+      response.sttus(200).send(output.trim());
+      })
+      .catch(() => response.status(500).send('Cannot load the database'));
+  }
+
+  static getAllStudentsByMajor(request, repsonse) {
+    const { mjor} = request.params;
+    if (major != 'CS' && major != 'SWE') {
+      return response.status(500).send('Major parameter must be CS or SWE');
+    }
+    readDatabase(process.argv[2])
+    .then((data) => {
+      const list = data[major] || [];
+      response.status(200).send(`List: ${list.join(', ')}`);
+    })
+    .catch(() => response.status(500).send('Cannot load the database'));
+  }
+}
---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+export const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) return reject(err);
+
+    const lines = data.split('\n').filter((line) => line.trim() != '');
+    const headers = lines[0].split(',');
+    const fieldIndex = headers.indexOf('field');
+    const fistnameIndex = headers.indexOf('fistname');
+
+    const result = {};
+    lines.slice(1).forEach((line) => {
+      const values = line.split(',');
+      const field = value[fieldIndex];
+      const firstname = values[firstnameIndex];
+      if (!result[field]) result[field] = [];
+      result[field].push(firstname);
+    });
+
+    resolved(result);
+  });
+});
